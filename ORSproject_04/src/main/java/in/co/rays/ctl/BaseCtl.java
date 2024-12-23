398434b93160daee8153c528217dbaa4efa41a26
sandeep gurjar <sandeepgurjardogne202@gmail.com>
2024-12-23 18:21:45 +0530

    done

--- ORSproject_04/src/main/java/in/co/rays/ctl/BaseCtl.java
+++
@@ -1,9 +0,0 @@
-package in.co.rays.ctl;
-
-public class BaseCtl {
-
-	// Message Constants
-	public static final String MSG_SUCCESS = "success";
-	public static final String MSG_ERROR = "error";
-
-}
